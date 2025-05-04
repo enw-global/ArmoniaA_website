1e937137f413025209d8105c15b8f0ea9eb72ae2
Joshua. C <76528955+jemcap@users.noreply.github.com>
2025-05-04 16:45:00 +0100

    Implemented Hero Video on Landing page

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from "react";
+import { sanityClient } from "../../lib/sanity";
+
+interface VideoAsset {
+  _id: string;
+  title: string;
+  url: string;
+}
+
 const Hero = () => {
-  return <section className="h-screen bg-black">Hero</section>;
+  const [video, setVideo] = useState<string | null>(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch<VideoAsset[]>(
+        `*[_type == "videoAsset"]{ title, "url": videoFile.asset->url }`
+      )
+      .then((results) => {
+        if (results.length > 0) {
+          setVideo(results[0].url);
+        }
+      })
+      .catch((err) => console.error("Sanity failed to fetch data:", err));
+  }, []);
+  if (!video) return <p>Loading video…</p>;
+  return (
+    <section className="h-screen bg-black flex justify-center">
+      <video
+        controls
+        autoPlay
+        muted
+        controlsList="nodownload nofullscreen noremoteplayback"
+        disablePictureInPicture
+        className="max-w-[100%]"
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+    </section>
+  );
 };
 
 export default Hero;
