aca69f8aea14c2054f09a5dcf939cdff70873835
Josh Capito <joshuaecapito22@gmail.com>
2025-11-24 12:29:40 +0000

    fix: Changed assetFiles type to array of type image

--- armoniaa/schemaTypes/archiveAssets.ts
+++ armoniaa/schemaTypes/archiveAssets.ts
@@ -1,49 +1,49 @@
 import {defineType} from 'sanity'
 
 export default defineType({
-    name: 'archiveAssets',
-    title: 'Archive Assets',
-    type: 'document',
-    fields: [
+  name: 'archiveAssets',
+  title: 'Archive Assets',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'peopleInvolved',
+      title: 'People Involved',
+      type: 'array',
+      of: [{type: 'string'}],
+    },
+    {
+      name: 'archiveDate',
+      title: 'Archive Date',
+      type: 'number',
+      validation: (Rule) => Rule.min(1900).max(2100),
+    },
+    {
+      name: 'projectType',
+      title: 'Project Type',
+      type: 'string',
+    },
+    {
+      name: 'assetFiles',
+      title: 'Asset Files',
+      type: 'array',
+      of: [
         {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
+          type: 'image',
+          options: {
+            accept: 'image/tiff, image/svg+xml, image/png, image/jpeg, image/gif',
+          },
         },
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'text',
-        },
-        {
-            name: 'peopleInvolved',
-            title: 'People Involved',
-            type: 'array',
-            of: [{type: 'string'}],
-        },
-        {
-            name: 'archiveDate',
-            title: 'Archive Date',
-            type: 'number',
-            validation: (Rule) => Rule.min(1900).max(2100),
-        },
-        {
-            name: 'projectType',
-            title: 'Project Type',
-            type: 'string',
-        },
-        {
-            name: 'assetFiles',
-            title: 'Asset Files',
-            type: 'array',
-            of: [
-                {
-                    type: 'file',
-                    options: {
-                        accept: 'image/tiff, image/svg+xml, image/png, image/jpeg, image/gif',
-                    },
-                },
-            ],
-        },
-    ],
+      ],
+    },
+  ],
 })
