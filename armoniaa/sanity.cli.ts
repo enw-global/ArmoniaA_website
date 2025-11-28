443e43d19d3027ddc9605ad209462f4cd0a4b148
Josh Capito <joshuaecapito22@gmail.com>
2025-11-28 17:49:43 +0000

    fix: Changed sanity videoAsset schema type to url

--- armoniaa/sanity.cli.ts
+++ armoniaa/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'kfip38fi',
+    projectId: 'eni40kd7',
     dataset: 'production'
   },
   /**
--- armoniaa/schemaTypes/videoAsset.ts
+++ armoniaa/schemaTypes/videoAsset.ts
@@ -1,22 +1,21 @@
-import { defineType } from "sanity";
+import {defineType} from 'sanity'
 
 export default defineType({
-    name: 'videoAsset',
-    title: 'Video Asset',
-    type: 'document',
-    fields: [
-        {
-            name: 'title',
-            title: 'Title',
-            type: 'string'
-        },
-        {
-            name: 'videoFile',
-            title: 'Video File',
-            type: 'file',
-            options: {
-                accept: 'video/mp4, video/quicktime'
-            }
-        },
-    ]
+  name: 'videoAsset',
+  title: 'Video Asset',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'videoFile',
+      title: 'Video File',
+      description: 'URL to the video file from S3',
+      type: 'url',
+      validation: (Rule) => Rule.uri({scheme: ['http', 'https']}),
+    },
+  ],
 })
